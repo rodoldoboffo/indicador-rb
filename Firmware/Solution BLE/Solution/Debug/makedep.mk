8d2bb4d382db30499b2a0885cdfe561112d75825
Rodolfo <rodolfoboffo@gmail.com>
2019-02-18 13:54:56 -0300

    Limpando funcoes inutilizadas após a inclusao do Bluetooth

--- Firmware/Solution BLE/Solution/Debug/makedep.mk
+++ Firmware/Solution BLE/Solution/Debug/makedep.mk
@@ -4,27 +4,11 @@
 
 ads1231.c
 
-ds3231.c
-
-least_square.c
-
 main.c
-
-myeeprom.c
-
-mystdlib.c
-
-mystring.c
-
-nx4832t035.c
 
 indicator.c
 
-passive_buzer.c
-
-serial.c
-
-software_uart.c
+mystdlib.c
 
 software_uart_ble.c
 
