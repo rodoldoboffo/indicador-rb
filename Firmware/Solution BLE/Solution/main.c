b5388f28f5b6891245e74ef1bb057fba69a326de
Rodolfo <rodolfoboffo@gmail.com>
2019-03-13 19:52:53 -0300

    Ajustando intervalo do loop principal, para ser razoavelmente menor que o intervalo das coletas do app android

--- Firmware/Solution BLE/Solution/main.c
+++ Firmware/Solution BLE/Solution/main.c
@@ -15,6 +15,8 @@ int main(void)
 	while (1)
 	{
 		processBLEMessages();
-		_delay_ms(10);
+		// Aparentemente esse tempo deve ser razoavelmente menor que o intervalo
+		// de coleta do app android
+		_delay_ms(5);
 	}
 }
